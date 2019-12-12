77858d4e26bbdcb157c7b56d3b4872594b949b5c
hiromatsu-kodai <57866433+hiromatsu-kodai@users.noreply.github.com>
2019-12-12 19:21:49 +0900

    送られたデータによって表内の表示をかえる

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </tr>
 <tr>
 <td>0000</td><td>A</td><td><?php
-  echo $_POST["number"] ."さんのメールアドレスは". $_POST["name"] ."です。";
+  //echo $_POST["number"] ."さんのメールアドレスは". $_POST["name"] ."です。";
   //$name1=$_POST["number"];
   //$number1=$_POST["name"];
   if($_POST["name"]=="A" && $_POST["number"]=="0000")
