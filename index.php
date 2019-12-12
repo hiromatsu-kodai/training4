479498fbfd7514b6a7ce3f3922bf490a29553853
hiromatsu-kodai <57866433+hiromatsu-kodai@users.noreply.github.com>
2019-12-12 20:07:00 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 <tr>
 <td>0000</td><td>A</td><td><?php
   $a=0;
-  if($a==0)
-     if($_POST["name"]=="A" && $_POST["number"]=="0000")
+  if($a==1)
+     elseif($_POST["name"]=="A" && $_POST["number"]=="0000")
         echo"○";
         $a=1;
      else
@@ -25,8 +25,8 @@
 <tr> 
 <td>0001</td><td>B</td><td><?php  
   $b=0;
-  if($b==0)
-     if($_POST["name"]=="B" && $_POST["number"]=="0001")
+  if($b==1)
+     elseif($_POST["name"]=="B" && $_POST["number"]=="0001")
         echo"○";
         $b=1;
      else
@@ -37,8 +37,8 @@
 <tr>
 <td>0002</td><td>C</td><td><?php  
   $c=0;
-  if($c==0)
-     if($_POST["name"]=="C" && $_POST["number"]=="0002")
+  if($c==1)
+     elseif($_POST["name"]=="C" && $_POST["number"]=="0002")
         echo"○";
         $c=1;
      else
